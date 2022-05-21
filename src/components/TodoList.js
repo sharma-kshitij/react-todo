b72342fd9d375c14ba3aedaf856a793de2056ecd
Kshitij Sharma <sharmakshitij250@gmail.com>
2022-05-21 14:09:48 +0530

    Finish initial setup

---
+++ src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Todo from './Todo'
+
+export default function TodoList() {
+  return (
+      <h1>
+          TodoList Component
+          <Todo />
+      </h1>
+  )
+}
