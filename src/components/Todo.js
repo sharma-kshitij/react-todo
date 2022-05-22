afc93f6a2e0f0d8e08be7ad201ad1e27a304db0c
Kshitij Sharma <sharmakshitij250@gmail.com>
2022-05-22 11:43:06 +0530

    Add delete funcionality on completion

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,18 +1,32 @@
 import React from 'react'
 
-export default function Todo({text}) {
+export default function Todo({ todo, todos, setTodos, text, key }) {
+
+  // const completeHandler = () => {
+  //   setTodos(todos.map((item) => {
+  //     if (item.id === todo.id) {
+  //       return {
+  //         ...item,
+  //         completed: !item.completed,
+  //       }
+  //     }
+  //     return item
+  //   }))
+  // }
+
+  const deleteHandler = () =>{
+    setTodos(todos.filter((item => item.id !== todo.id)));
+  }
+
   return (
     <div className='btn-contain'>
-        
-        <button className='add-btn'>Completed</button>
-        <li className='todo-list-item'>
-          <h4 className='todo'>
-            {text}
-          </h4>
-        </li>
-        
-        
-        
+
+      <button onClick={deleteHandler} className='add-btn'>Completed</button>
+      <li className="todo-list-item">
+        <h4 className={`todo ${todo.completed ? "completed" : ''}`}>
+          {text}
+        </h4>
+      </li>
     </div>
   )
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,11 +3,14 @@ import Todo from './Todo'
 
 export default function TodoList({todos,setTodos}) {
   return (
-      <div>
-          <ul className='todo-list'>
+      <div className='todo-list'>
+          <ul>
               {
                    todos.map(todo => (
                     <Todo
+                    todo = {todo}
+                    todos = {todos}
+                    setTodos = {setTodos}
                     text={todo.text} 
                     key={todo.id} />
                 ))
